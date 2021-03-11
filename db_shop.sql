3a55338eada36554c972090d0e30f048e8a58235
BVNKFATBOT <holyzstudio@gmail.com>
2021-03-11 08:52:32 +0700

    Update db_shop.sql

--- db_shop.sql
+++ db_shop.sql
@@ -1,7 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 11, 2021 at 02:50 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_shop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account`
+--
 
 CREATE TABLE `account` (
   `acc_id` int(5) NOT NULL,
@@ -13,6 +37,11 @@ CREATE TABLE `account` (
   `acc_status` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
 
 CREATE TABLE `orders` (
   `order_id` int(5) NOT NULL,
@@ -21,10 +50,19 @@ CREATE TABLE `orders` (
   `order_address` text NOT NULL,
   `order_email` text NOT NULL,
   `order_tal` text NOT NULL,
-  `order_key` text NOT NULL
+  `order_key` text NOT NULL,
+  `status` text NOT NULL,
+  `order_track` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `orders`
+--
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders_detail`
 --
 
 CREATE TABLE `orders_detail` (
@@ -32,6 +70,22 @@ CREATE TABLE `orders_detail` (
   `order_id` int(5) NOT NULL,
   `pro_id` int(5) NOT NULL,
   `qty` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `pay_id` int(5) NOT NULL,
+  `pay_date` datetime NOT NULL,
+  `pay_name` text NOT NULL,
+  `pay_bank` text NOT NULL,
+  `order_key` text NOT NULL,
+  `pay_status` text NOT NULL,
+  `pay_img` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -50,7 +104,12 @@ CREATE TABLE `product` (
   `pro_type` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Indexes for dumped tables
+--
 
+--
+-- Indexes for table `account`
 --
 ALTER TABLE `account`
   ADD PRIMARY KEY (`acc_id`);
@@ -68,6 +127,12 @@ ALTER TABLE `orders_detail`
   ADD PRIMARY KEY (`detail_id`);
 
 --
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`pay_id`);
+
+--
 -- Indexes for table `product`
 --
 ALTER TABLE `product`
@@ -78,6 +143,35 @@ ALTER TABLE `product`
 --
 
 --
+-- AUTO_INCREMENT for table `account`
+--
+ALTER TABLE `account`
+  MODIFY `acc_id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders_detail`
+--
+ALTER TABLE `orders_detail`
+  MODIFY `detail_id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `pay_id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `pro_id` int(5) NOT NULL AUTO_INCREMENT;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
